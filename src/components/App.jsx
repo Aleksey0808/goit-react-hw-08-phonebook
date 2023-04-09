597580ea61cd68ec75753aa573ccf66e32cc8eb8
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-04-09 21:19:40 +0300

    0904232

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import { getContactsThunk } from '../redux/operations/contactsThunk';
 
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { Container, Title } from './App.styles';
+import { Register } from '../pages/Register';
 
 export function App() {
   const dispatch = useDispatch();
@@ -21,6 +23,10 @@ export function App() {
       <Title>Contacts</Title>
       <Filter />
       <ContactList />
+
+      <Routes>
+        <Route path="/register" component={Register} />
+      </Routes>
     </Container>
   );
 }
