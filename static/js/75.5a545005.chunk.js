e9bbb6fd99aa69b65f2dfd0dcdb028563de62ad9
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-04-14 18:38:41 +0000

    Deploying to gh-pages from @ Aleksey0808/goit-react-hw-08-phonebook@1f8e0fc5145afa19456dd2d2852cb37e0c93110d 🚀

--- static/js/75.5a545005.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{6075:function(e,t,o){o.r(t);var r=o(1087),n=o(184);t.default=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h2",{children:"Welcome to the phonebook"}),(0,n.jsx)(r.OL,{to:"/register",children:" Do you a have account?"}),(0,n.jsx)(r.OL,{to:"/login",children:"Or registration?"})]})}}}]);
-//# sourceMappingURL=75.5a545005.chunk.js.map
