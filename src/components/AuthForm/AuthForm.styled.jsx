3b03c640bec0bf3cab078b2d42e343566fee75dd
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-04-14 22:11:23 +0300

    add styled

--- src/components/AuthForm/AuthForm.styled.jsx
+++ src/components/AuthForm/AuthForm.styled.jsx
@@ -1,19 +1,13 @@
-import styled from '@emotion/styled'
-import { NavLink } from "react-router-dom";
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
   text-decoration: none;
   padding: 12px;
   font-weight: 700;
-  // color: var(--primaryTextColor);
+  color: #000000;
 
-  
-  &:not(:last-child){
-   margin-right: 12px;
+  &:not(:last-child) {
+    margin-right: 12px;
   }
-  transition: all 0.5s ease;
-  text-shadow: 2px 2px 4px blue;
-  // &.active {
-  //   color: var(--secondaryTextColor);
-  // }
 `;
--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -2,11 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { authSelectors } from 'redux/auth';
 
-function PublicRoute({
-  children,
-  restricted = false,
-  redirectTo = '/',
-}) {
+function PublicRoute({ children, restricted = false, redirectTo = '/' }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const shouldRedirect = isLoggedIn && restricted;
   return <>{shouldRedirect ? <Navigate to={redirectTo} /> : children}</>;
